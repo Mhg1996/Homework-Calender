64ef9067a744ecf4b00d1e753da53c22827f8fec
Marissa Gonsiewski <mhgonsiewski@gmail.com>
2021-02-27 20:33:27 -0500

    added one row, time, and beginning of javascript

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var today = new Date();
+
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+var today = new Date();
+
+var time = today.getHours()  + today.getMinutes()  + today.getSeconds();
